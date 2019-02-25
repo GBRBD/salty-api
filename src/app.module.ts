3094353449f3f7baddf4911176733209225b160d
GBRBD <bodigaborke@gmail.com>
2019-02-25 19:00:05 +0100

    Fix env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FeatureModule } from './feature/feature.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from './config.module';
-import { ConfigService } from './config.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      process.env.MONGO || new ConfigService('development.env').get('MONGO'),
-      {
-        useNewUrlParser: true,
-      },
-    ),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+    }),
     FeatureModule,
-    ConfigModule,
   ],
 })
 export class AppModule {}
